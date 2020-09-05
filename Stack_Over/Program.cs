536fd3a8c46b061f43c9853b2aa9dd73dd34db2b
Ganesh Kadam <ganesh89kadam@gmail.com>
2020-09-05 08:38:50 +0530

    commit

---
+++ Stack_Over/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stack_Over
+{
+    static void Main(string[] args)
+    {
+        var newPost = new Post
+        {
+            Title = "HELLO WORLD",
+            Description = "This is some awesome hello world description."
+        };
+
+        Console.WriteLine(newPost.Title);
+        Console.WriteLine(newPost.Description);
+        Console.WriteLine(newPost.CreationDateTime);
+
+        newPost.UpVote();
+        newPost.UpVote();
+        newPost.DownVote();
+
+        Console.WriteLine("Number of votes: " + newPost.DisplayVotes());
+    }
+}
